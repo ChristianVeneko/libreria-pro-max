e176e532c419e7afe5040e4c51057dfe3bba7445
Christian Mendoza <christianmendozater@gmail.com>
2025-11-07 11:22:46 -0400

    Implement CRUD operations in LibroService and add corresponding methods in LibroController for book management

--- src/controllers/libroController.js
+++ src/controllers/libroController.js
@@ -21,6 +21,51 @@ class LibroController {
         }
     }
 
+    // GET: obtener un libro por su ID
+    async getlibroById(req, res, next) {
+        try{
+            const {id} = req.params;
+            const result = await LibroService.getlibroById(id);
+            res.json(result);
+        }catch(error){
+        }
+    }
+    
+
+    // POST: crear un nuevo libro
+    async createLibro(req, res, next){
+        try{
+            //req.validateData viene del middleware de validacion
+            const result = await LibroService.createLibro(req.validateData);
+            res.status(201).json(result); 
+
+        }catch(error){
+            next(error);
+        }
+    }
+
+    // PUT: actualizar un libro
+    async updateLibro(req, res, next){
+        try{
+            const {id} = req.params;
+            const result = await LibroService.updateLibro(id, req.validateData);
+            res.json(result);
+        }catch(error){
+            next(error);
+        }
+    }
+
+    // DELETE: eliminar un libro
+    async deleteLibro(req, res, next){
+        try{
+            const {id} = req.params;
+            const result = LibroService.deleteLibro(id);
+            res.json(result);
+        }catch(error){
+            next(error);
+        }
+    }
+
 }
 
 module.exports = new LibroController();
--- src/services/LibroService.js
+++ src/services/LibroService.js
@@ -3,14 +3,69 @@ const LibroRepository = require('../repositories/LibroRepository');
 class LibroService {
     //obtener todos los libros
     async getAllLibros(filters) {
-        try{
+        try {
             const libros = await LibroRepository.findAll(filters);
-        return {
-            status : 'success',
-            data: libros,
-            total: libros.length,
-        };
+            return {
+                status: 'success',
+                data: libros,
+                total: libros.length,
+            };
         } catch (error) {
+            throw error;
+        }
+    }
+
+    //obtener libro por id
+    async getLibroById(cod_libro) {
+        try {
+            const libro = await LibroRepository.findById(cod_libro);
+            return {
+                status: 'success',
+                data: libro,
+            };
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //crear un libro 
+    async createLibro(libroData) {
+        try{
+            const cod_libro = await LibroRepository.create(libroData);
+            const libro = await LibroRepository.findById(cod_libro);
+            return{
+                status: 'success',
+                message: 'Libro creado exitosamente',
+                data: libro,
+            };
+        }catch(error){
+            throw error;
+        }   
+    }
+
+    //actualizar un libro
+    async updateLibro(cod_libro, libroData){
+        try{
+            const libro = await LibroRepository.update(cod_libro, libroData);
+            return {
+                status: 'success',
+                message: 'Libro actualizado exitosamente',
+                data: libro,
+            };
+        }catch(error){
+            throw error;
+        }
+    }
+
+    //eliminar un libro
+    async deleteLibro(cod_libro){
+        try{
+            await LibroRepository.delete(cod_libro);
+            return{
+                status: 'success',
+                message: 'Libro eliminado exitosamente',
+            };
+        }catch(error){
             throw error;
         }
     }
